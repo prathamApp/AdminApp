2e4b4203ab685396d9f88f0b0a03d99658a692e1
prathamApp <prathambaner123@gmail.com>
2018-07-27 11:56:20 +0530

    27 July course completion --- in progress student attendance --- Change Spinner to multiselect spinner --- done Submit data on click --- autpop turn on internet ---

--- app/src/main/java/com/pratham/admin/forms/CourseCompletionForm.java
+++ app/src/main/java/com/pratham/admin/forms/CourseCompletionForm.java
@@ -4,15 +4,24 @@ import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import com.pratham.admin.R;
+import com.pratham.admin.database.AppDatabase;
+import com.pratham.admin.modalclasses.Groups;
+import com.pratham.admin.modalclasses.Village;
+import com.pratham.admin.util.CustomGroup;
 import com.pratham.admin.util.DatePickerFragmentOne;
 import com.pratham.admin.util.DatePickerFragmentTwo;
 import com.pratham.admin.util.Utility;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -22,14 +31,10 @@ public class CourseCompletionForm extends AppCompatActivity {
 
     @BindView(R.id.sp_Village)
     Spinner sp_Village;
-    @BindView(R.id.rg_Groups)
-    RadioGroup rg_Groups;
-    @BindView(R.id.rg_CourseOne)
-    RadioGroup rg_CourseOne;
-    @BindView(R.id.rg_TopicOne)
-    RadioGroup rg_TopicOne;
-    @BindView(R.id.rg_CourseTwo)
-    RadioGroup rg_CourseTwo;
+    @BindView(R.id.sp_Groups)
+    Spinner sp_Groups;
+
+
     @BindView(R.id.rg_Event)
     RadioGroup rg_Event;
     @BindView(R.id.edt_ParentCount)
@@ -40,6 +45,11 @@ public class CourseCompletionForm extends AppCompatActivity {
     Button btn_DatePickerTwo;
     @BindView(R.id.btn_Submit)
     Button btn_Submit;
+
+    List<Village> villageList = new ArrayList<>();
+    List<Groups> AllGroupsInDB;
+    List registeredGRPs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +65,14 @@ public class CourseCompletionForm extends AppCompatActivity {
         btn_DatePicker.setPadding(8, 8, 8, 8);
         btn_DatePickerTwo.setText(new Utility().GetCurrentDate().toString());
         btn_DatePickerTwo.setPadding(8, 8, 8, 8);
+
+        //retrive all groups from  DB
+        AllGroupsInDB = AppDatabase.getDatabaseInstance(this).getGroupDao().getAllGroups();
+
+        // Populate Village Spinner
+        villageList = AppDatabase.getDatabaseInstance(this).getVillageDao().getAllVillages();
+        populateVillages();
+
     }
 
     @OnClick(R.id.btn_DatePicker)
@@ -68,5 +86,63 @@ public class CourseCompletionForm extends AppCompatActivity {
         DialogFragment newFragment = new DatePickerFragmentTwo();
         newFragment.show(getFragmentManager(), "DatePicker");
     }
+
+    private void populateVillages() {
+        final List VillageName = new ArrayList();
+        if (!villageList.isEmpty()) {
+            VillageName.add(new CustomGroup("Select Village"));
+            for (int j = 0; j < villageList.size(); j++) {
+                CustomGroup customGroup = new CustomGroup(villageList.get(j).getVillageName(), villageList.get(j).getVillageId());
+                VillageName.add(customGroup);
+            }
+            ArrayAdapter villageAdapter = new ArrayAdapter(CourseCompletionForm.this, android.R.layout.simple_spinner_dropdown_item, VillageName);
+            sp_Village.setAdapter(villageAdapter);
+        }
+
+        sp_Village.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                CustomGroup customGroup = (CustomGroup) VillageName.get(pos);
+                String vid = customGroup.getId();
+
+                // Populate Registered Groups Spinner
+                populateRegisteredGroups(vid);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private void populateRegisteredGroups(String villageID) {
+        // todo get registered grps
+        registeredGRPs = new ArrayList();
+        registeredGRPs.add(new CustomGroup("Select Groups"));
+        if (AllGroupsInDB != null) {
+            for (int i = 0; i < AllGroupsInDB.size(); i++) {
+                if (AllGroupsInDB.get(i).getVillageId().equals(villageID))
+                    registeredGRPs.add(new CustomGroup(AllGroupsInDB.get(i).getGroupName(), AllGroupsInDB.get(i).getGroupId()));
+            }
+        }
+
+        ArrayAdapter grpAdapter = new ArrayAdapter(CourseCompletionForm.this, android.R.layout.simple_spinner_dropdown_item, registeredGRPs);
+        sp_Groups.setAdapter(grpAdapter);
+        sp_Groups.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                CustomGroup customGroup = (CustomGroup) registeredGRPs.get(pos);
+                String groupId = customGroup.getId();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+
 
 }
